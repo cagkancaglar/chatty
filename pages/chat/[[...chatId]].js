419ed5f4b3e410285c7b3c8ecc6fc6b2c311b11e
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-11-17 01:11:35 +0300

    Add: ChatSidebar

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,3 +1,4 @@
+import { ChatSidebar } from "components/ChatSidebar";
 import Head from "next/head";
 import React from "react";
 
@@ -8,7 +9,7 @@ export default function Chat() {
         <title>New Chat</title>
       </Head>
       <div className="grid h-screen grid-cols-[260px_1fr]">
-        <div className="">sidebar</div>
+        <ChatSidebar />
         <div className="flex flex-col bg-gray-700">
           <div className="flex-1">chat window</div>
           <footer className="bg-gray-800 p-10">footer</footer>
