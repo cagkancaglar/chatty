480f65ad5dec7729796f9cd949b7fb1b148b4e51
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-11-21 13:07:58 +0300

    Add: addMessageToChat endpoint

--- pages/api/chat/sendMessage.js
+++ pages/api/chat/sendMessage.js
@@ -44,8 +44,25 @@ export default async function handler(req) {
         }),
       },
       {
+        onBeforeStream: async ({ emit }) => {
+          emit(chatId, "newChatId");
+        },
         onAfterStream: async ({ fullContent }) => {
-
+          await fetch(
+            `${req.headers.get("origin")}/api/chat/addMessageToChat`,
+            {
+              method: "POST",
+              headers: {
+                "content-type": "application/json",
+                cookie: req.headers.get("cookie"),
+              },
+              body: JSON.stringify({
+                chatId,
+                role: "assistant",
+                content: fullContent,
+              }),
+            }
+          );
         },
       }
     );
