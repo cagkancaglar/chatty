484e61fb04ba3bac1ebea7dbec360edb176d3ade
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-11-25 00:35:44 +0300

    Add: Validating endpoints

--- pages/api/chat/sendMessage.js
+++ pages/api/chat/sendMessage.js
@@ -7,6 +7,19 @@ export const config = {
 export default async function handler(req) {
   try {
     const { chatId: chatIdFromParam, message } = await req.json();
+
+    // validate message data
+    if (!message || typeof message !== "string" || message.length > 200) {
+      return new Response(
+        {
+          message: "Message is required and must be less than 200 characters!",
+        },
+        {
+          status: 422,
+        }
+      );
+    }
+
     let chatId = chatIdFromParam;
     const initialChatMessage = {
       role: "system",
@@ -112,6 +125,9 @@ export default async function handler(req) {
     );
     return new Response(stream);
   } catch (error) {
-    console.log("An error occured in send message: ", error);
+    return new Response(
+      { message: "An error occured in sendMessage!" },
+      { status: 500 }
+    );
   }
 }
